b7633f27b1e6faf9b3779ac11800f2447323a0ac
Justin <justin@justintimetechnology.com>
2014-12-10 11:40:16 -0500

    sprite work + beginning of code

--- Final Resources/PokemonRedux/src/ofApp.cpp
+++ Final Resources/PokemonRedux/src/ofApp.cpp
@@ -2,7 +2,11 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    //george.loadImage("barrel.png");
+    ralph.loadImage("006.PNG");
+    larry.loadImage("pallet.bmp");
+    j=0;
+    k=0;
 }
 
 //--------------------------------------------------------------
@@ -12,11 +16,44 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    int i;//,j=0,k=0;
+
+    /*for(i=0;i<300;i++)
+    {
+        ofPushMatrix();//change coordinate matrix, works in conjunction with ofTranslate()
+        k++;
+        if((i*16)%160==0)
+        {
+            j++;
+            k=0;
+        }
+        ofTranslate(k*16,(j-1)*16);//shifts image
+        george.draw(0,0);//draws image
+     ofPopMatrix();//change coordinate matrix, works in conjunction with ofTranslate()
+     }*/
+     larry.draw(j,k);
+     ralph.draw(80,64);
 
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
+    if(key == 'a')
+    {
+        j=j+16;
+    }
+    else if(key=='d')
+    {
+        j=j-16;
+    }
+    else if(key=='w')
+    {
+        k=k+16;
+    }
+    else if(key=='s')
+    {
+        k=k-16;
+    }
 
 }
 
@@ -56,6 +93,13 @@ void ofApp::gotMessage(ofMessage msg){
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo){
 
+}
+
+Pokemon::Pokemon(int baseHP_in, int baseAtk_in, int baseDef_in, int baseSpd_in, int baseSpec_in, int type1_in, int type2_in, int evoLvl_in, int expClass_in)
+{
+    baseHP=baseHP_in;
+    baseAtk=baseAtk_in;
+    baseDef=baseDef_in
 }
--- Final Resources/PokemonRedux/src/ofApp.h
+++ Final Resources/PokemonRedux/src/ofApp.h
@@ -18,5 +18,77 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		
+
+		ofImage george;
+		ofImage ralph;
+		ofImage larry;
+		int i,j,k;
+
 };
+
+class Effect{
+    private:
+        int index;
+        int damage;
+        int heal;
+        int atk;
+        int def;
+        int spd;
+        int spec;
+};
+
+class Move{
+    private:
+        Effect effect;
+        int power;
+        string name;
+    public:
+        Move(Effect effect_in, int pow_in, string name_in);
+        Move();
+        setMove(Effect effect_in, int pow_in, string name_in);
+        setMove()
+};
+
+class Pokemon{
+    private:
+        int idNum;
+        string name;
+        int baseHP;     //base Health Point stat
+        int baseAtk;    //base Attack stat
+        int baseDef;    //base Defense stat
+        int baseSpd;    //base Speed stat
+        int baseSpec;   //base Special stat
+        int type1;      //first elemental type
+        int type2;      //second elemental type
+        int evoLvl;     //level at which pokemon evolves (00 if pokemon does not evolve)
+        int expClass;   //which level/experience ratio to use to determine pokemon level
+
+        Pokemon(int baseHP_in, int baseAtk_in, int baseDef_in, int baseSpd_in, int baseSpec_in, int type1_in, int type2_in, int evoLvl_in, int expClass_in);
+        Pokemon();
+        void setPoke(int baseHP_in, int baseAtk_in, int baseDef_in, int baseSpd_in, int baseSpec_in, int type1_in, int type2_in, int evoLvl_in, int expClass_in);
+};
+
+class PartyPokemon{
+    private:
+        int maxHP;
+        int atkStat;
+        int defStat;
+        int spdStat;
+        int specStat;
+        int move1;
+        int move2;
+        int move3;
+        int move4;
+        int exp;
+        int level;
+    public:
+        int hp;
+        int atk;
+        int def;
+        int spd;
+        int spec;
+        int faint; //1 or 0
+        PartyPokemon(Pokemon, int level)
+        void learnMove(Move newMove)
+};
+
